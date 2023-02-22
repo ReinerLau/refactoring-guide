845fff5bceb4dba9d307ba02701c3b775e7857d5
ReinerLau <lk850593913@gmail.com>
2023-02-22 15:29:43 +0800

    refactor(statement): 重命名 amountFor 函数返回值

--- src/statement.ts
+++ src/statement.ts
@@ -2,7 +2,7 @@
  * @Author: ReinerLau lk850593913@gmail.com
  * @Date: 2023-02-22 13:45:34
  * @LastEditors: ReinerLau lk850593913@gmail.com
- * @LastEditTime: 2023-02-22 15:25:19
+ * @LastEditTime: 2023-02-22 15:29:16
  * @FilePath: \refactoring-guide\src\statement.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -38,25 +38,26 @@ export function statement(invoice, plays) {
   // pref、play 是不会被修改的变量作为参数传递
   function amountFor(pref: any, play: any) {
     // 在里面初始化就在里面初始化
-    let thisAmount = 0;
+    let result = 0;
     switch (play.type) {
       case "tragedy":
-        thisAmount = 40000;
+        result = 40000;
         if (pref.audience > 30) {
-          thisAmount += 1000 * (pref.audience - 30);
+          result += 1000 * (pref.audience - 30);
         }
         break;
       case "comedy":
-        thisAmount = 30000;
+        result = 30000;
         if (pref.audience > 20) {
-          thisAmount += 10000 + 500 * (pref.audience - 20);
+          result += 10000 + 500 * (pref.audience - 20);
         }
-        thisAmount += 300 * pref.audience;
+        result += 300 * pref.audience;
         break;
       default:
         throw new Error(`unknown type: ${play.type}`);
     }
     // thisAmount 是被修改的变量作为函数的结果返回
-    return thisAmount;
+    // 永远将函数的返回值命名为 result，方便一眼看出作用
+    return result;
   }
 }
