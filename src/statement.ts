deec38472aa15a99e2273c205be973fdf6cec50f
ReinerLau <lk850593913@gmail.com>
2023-02-22 15:42:53 +0800

    refactor(statement): amountFor 内联变量取代参数

--- src/statement.ts
+++ src/statement.ts
@@ -2,7 +2,7 @@
  * @Author: ReinerLau lk850593913@gmail.com
  * @Date: 2023-02-22 13:45:34
  * @LastEditors: ReinerLau lk850593913@gmail.com
- * @LastEditTime: 2023-02-22 15:39:20
+ * @LastEditTime: 2023-02-22 15:41:10
  * @FilePath: \refactoring-guide\src\statement.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -19,10 +19,11 @@ export function statement(invoice, plays) {
   for (let pref of invoice.performances) {
     let thisAmount = 0;
 
-    thisAmount = amountFor(pref, playFor(pref));
+    thisAmount = amountFor(pref);
 
     volumeCredits += Math.max(pref.audience - 30, 0);
-    if ("comedy" === playFor(pref).type) volumeCredits += Math.floor(pref.audience / 5);
+    if ("comedy" === playFor(pref).type)
+      volumeCredits += Math.floor(pref.audience / 5);
 
     result += `${playFor(pref).name}: ${format(thisAmount / 100)} (${
       pref.audience
@@ -41,10 +42,10 @@ export function statement(invoice, plays) {
   // 提炼函数，
   // pref、play 是不会被修改的变量作为参数传递
   // 重命名 pref - aPerformance 使可读性更好
-  function amountFor(aPerformance: any, play: any) {
+  function amountFor(aPerformance: any) {
     // 在里面初始化就在里面初始化
     let result = 0;
-    switch (play.type) {
+    switch (playFor(aPerformance).type) {
       case "tragedy":
         result = 40000;
         if (aPerformance.audience > 30) {
@@ -59,7 +60,7 @@ export function statement(invoice, plays) {
         result += 300 * aPerformance.audience;
         break;
       default:
-        throw new Error(`unknown type: ${play.type}`);
+        throw new Error(`unknown type: ${playFor(aPerformance).type}`);
     }
     // thisAmount 是被修改的变量作为函数的结果返回
     // 永远将函数的返回值命名为 result"
