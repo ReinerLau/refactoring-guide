f48664d1e044e7b9e1d21cb2e323d6158b86a9fa
ReinerLau <lk850593913@gmail.com>
2023-02-22 16:07:44 +0800

    refactor(statement): 内联变量

--- src/statement.ts
+++ src/statement.ts
@@ -2,7 +2,7 @@
  * @Author: ReinerLau lk850593913@gmail.com
  * @Date: 2023-02-22 13:45:34
  * @LastEditors: ReinerLau lk850593913@gmail.com
- * @LastEditTime: 2023-02-22 16:05:58
+ * @LastEditTime: 2023-02-22 16:07:27
  * @FilePath: \refactoring-guide\src\statement.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -16,9 +16,8 @@ export function statement(invoice, plays) {
     } seats)\n`;
     totalAmount += amountFor(pref);
   }
-  let volumeCredits = totalVolumeCredits();
   result += `Amount owed is ${usd(totalAmount)}\n`;
-  result += `You earned ${volumeCredits} credits\n`;
+  result += `You earned ${totalVolumeCredits()} credits\n`;
   return result;
 
   function totalVolumeCredits() {
