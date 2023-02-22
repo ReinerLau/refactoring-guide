5bf27d68016bc698d17e1c4ece8462bdad60fdd4
ReinerLau <lk850593913@gmail.com>
2023-02-22 15:47:11 +0800

    refactor(statement): 内联变量

--- src/statement.ts
+++ src/statement.ts
@@ -2,7 +2,7 @@
  * @Author: ReinerLau lk850593913@gmail.com
  * @Date: 2023-02-22 13:45:34
  * @LastEditors: ReinerLau lk850593913@gmail.com
- * @LastEditTime: 2023-02-22 15:41:10
+ * @LastEditTime: 2023-02-22 15:46:20
  * @FilePath: \refactoring-guide\src\statement.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -17,18 +17,14 @@ export function statement(invoice, plays) {
   }).format;
 
   for (let pref of invoice.performances) {
-    let thisAmount = 0;
-
-    thisAmount = amountFor(pref);
-
     volumeCredits += Math.max(pref.audience - 30, 0);
     if ("comedy" === playFor(pref).type)
       volumeCredits += Math.floor(pref.audience / 5);
 
-    result += `${playFor(pref).name}: ${format(thisAmount / 100)} (${
+    result += `${playFor(pref).name}: ${format(amountFor(pref) / 100)} (${
       pref.audience
     } seats)\n`;
-    totalAmount += thisAmount;
+    totalAmount += amountFor(pref);
   }
   result += `Amount owed is ${format(totalAmount / 100)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
