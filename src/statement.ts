4d3093f0755f83518cf46b18a573f43e29d3ed12
ReinerLau <lk850593913@gmail.com>
2023-02-22 16:48:16 +0800

    refactor(statement): 拆分阶段 - 中转数据结构 - 搬移函数

--- src/statement.ts
+++ src/statement.ts
@@ -2,7 +2,7 @@
  * @Author: ReinerLau lk850593913@gmail.com
  * @Date: 2023-02-22 13:45:34
  * @LastEditors: ReinerLau lk850593913@gmail.com
- * @LastEditTime: 2023-02-22 16:43:20
+ * @LastEditTime: 2023-02-22 16:47:45
  * @FilePath: \refactoring-guide\src\statement.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -15,12 +15,35 @@ export function statement(invoice, plays) {
   // 浅拷贝，目的是尽量不修改函数传进来的参数
   function enrichPerformance(aPerformance) {
     const result = Object.assign({}, aPerformance);
-    result.play = playFor(aPerformance)
+    result.play = playFor(aPerformance);
+    result.amount = amountFor(result);
     return result;
   }
-  // 以查询取代临时变量
+
   function playFor(aPerformance: any) {
     return plays[aPerformance.playID];
+  }
+
+  function amountFor(aPerformance: any) {
+    let result = 0;
+    switch (aPerformance.play.type) {
+      case "tragedy":
+        result = 40000;
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30);
+        }
+        break;
+      case "comedy":
+        result = 30000;
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 20);
+        }
+        result += 300 * aPerformance.audience;
+        break;
+      default:
+        throw new Error(`unknown type: ${aPerformance.play.type}`);
+    }
+    return result;
   }
 }
 
@@ -28,7 +51,7 @@ function renderPlainText(data: any, plays: any) {
   let result = `Statement for ${data.customer}\n`;
 
   for (let pref of data.performances) {
-    result += `${pref.play.name}: ${usd(amountFor(pref))} (${
+    result += `${pref.play.name}: ${usd(pref.amount)} (${
       pref.audience
     } seats)\n`;
   }
@@ -39,7 +62,7 @@ function renderPlainText(data: any, plays: any) {
   function totalAmount() {
     let result = 0;
     for (let pref of data.performances) {
-      result += amountFor(pref);
+      result += pref.amount;
     }
     return result;
   }
@@ -63,37 +86,7 @@ function renderPlainText(data: any, plays: any) {
   function volumeCreditsFor(pref: any) {
     let result = 0;
     result += Math.max(pref.audience - 30, 0);
-    if ("comedy" === pref.play.type)
-      result += Math.floor(pref.audience / 5);
-    return result;
-  }
-
-
-  // 提炼函数，
-  // pref、play 是不会被修改的变量作为参数传递
-  // 重命名 pref - aPerformance 使可读性更好
-  function amountFor(aPerformance: any) {
-    // 在里面初始化就在里面初始化
-    let result = 0;
-    switch (aPerformance.play.type) {
-      case "tragedy":
-        result = 40000;
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30);
-        }
-        break;
-      case "comedy":
-        result = 30000;
-        if (aPerformance.audience > 20) {
-          result += 10000 + 500 * (aPerformance.audience - 20);
-        }
-        result += 300 * aPerformance.audience;
-        break;
-      default:
-        throw new Error(`unknown type: ${aPerformance.play.type}`);
-    }
-    // thisAmount 是被修改的变量作为函数的结果返回
-    // 永远将函数的返回值命名为 result"
+    if ("comedy" === pref.play.type) result += Math.floor(pref.audience / 5);
     return result;
   }
 }
