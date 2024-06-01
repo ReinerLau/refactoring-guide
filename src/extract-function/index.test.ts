19f709e9069cec5872407b503a1fd678cbadba5c
reiner850593913 <lk850593913@gmail.com>
2024-06-01 15:55:44 +0800

    test(extract-function): with local variables

--- src/extract-function/index.test.ts
+++ src/extract-function/index.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it, vi } from "vitest";
-import { noLocalVariables } from ".";
+import { noLocalVariables, withLocalVariables } from ".";
 
 describe("提炼函数", () => {
   it("无局部变量", () => {
@@ -8,5 +8,14 @@ describe("提炼函数", () => {
     noLocalVariables();
 
     expect(log).toHaveBeenCalledWith("test");
+    expect(log).toHaveBeenCalledWith("reiner");
+  });
+
+  it("有局部变量", () => {
+    const log = vi.spyOn(console, "log");
+
+    withLocalVariables();
+
+    expect(log).toHaveBeenCalledWith("reiner");
   });
 });
--- src/extract-function/index.ts
+++ src/extract-function/index.ts
@@ -1,7 +1,24 @@
 export const noLocalVariables = () => {
+  const name = "reiner";
+
   print();
+  printName();
 
   function print() {
     console.log("test");
   }
+
+  function printName() {
+    console.log(name);
+  }
+};
+
+export const withLocalVariables = () => {
+  const name = "reiner";
+
+  printName(name);
+};
+
+const printName = (name: string) => {
+  console.log(name);
 };
