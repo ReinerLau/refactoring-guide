9a0ae6ab7a4e7f466b40e5553907a7ca380f2fe6
ReinerLau <lk850593913@gmail.com>
2024-06-10 13:26:40 +0800

    提炼函数: 有局部变量

--- src/extract-function.ts
+++ src/extract-function.ts
@@ -8,22 +8,28 @@ function printOwing(invoice) {
     outstanding += o.amount;
   }
 
-  // record due date
-  const today = Clock.today;
-  invoice.dueDate = new Date(
-    today.getFullYear(),
-    today.getMonth(),
-    today.getDate() + 30
-  );
+  recordDueDate(invoice);
 
-  // print details
-  console.log(`name: ${invoice.customer}`);
-  console.log(`amount: ${outstanding}`);
-  console.log(`due: ${invoice.dueDate.toLocalDateString()}`);
+  printDetails(invoice, outstanding);
 }
 
 function printBanner() {
   console.log("***********************");
   console.log("**** Customer Owes ****");
   console.log("***********************");
+}
+
+function printDetails(invoice, outstanding) {
+  console.log(`name: ${invoice.customer}`);
+  console.log(`amount: ${outstanding}`);
+  console.log(`due: ${invoice.dueDate.toLocalDateString()}`);
+}
+
+function recordDueDate(invoice) {
+  const today = Clock.today;
+  invoice.dueDate = new Date(
+    today.getFullYear(),
+    today.getMonth(),
+    today.getDate() + 30
+  );
 }
