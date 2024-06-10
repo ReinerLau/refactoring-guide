e94df35393a7cba18b81bfc138cccb1c8aff4f22
ReinerLau <lk850593913@gmail.com>
2024-06-10 17:27:12 +0800

    封装记录: 范例-3

--- src/encapsulate-record/index.ts
+++ src/encapsulate-record/index.ts
@@ -1,8 +1,21 @@
-const organization = { name: "Acme Gooseberries", country: "GB" };
+class Organization {
+  constructor(data) {
+    this._data = data;
+  }
+}
+
+const organization = new Organization({
+  name: "Acme Gooseberries",
+  country: "GB",
+});
 
 result += `<h1>${getRawDataOfOrganization().name}</h1>`;
 getRawDataOfOrganization().name = newName;
 
 function getRawDataOfOrganization() {
+  return organization._data;
+}
+
+function getOrganization() {
   return organization;
 }
