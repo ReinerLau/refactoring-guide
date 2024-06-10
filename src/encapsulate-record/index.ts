641c34ab0318691101f5428e1e765a59c3e798db
ReinerLau <lk850593913@gmail.com>
2024-06-10 17:23:38 +0800

    封装记录: 范例-2

--- src/encapsulate-record/index.ts
+++ src/encapsulate-record/index.ts
@@ -1,4 +1,8 @@
 const organization = { name: "Acme Gooseberries", country: "GB" };
 
-result += `<h1>${organization.name}</h1>`;
-organization.name = newName;
+result += `<h1>${getRawDataOfOrganization().name}</h1>`;
+getRawDataOfOrganization().name = newName;
+
+function getRawDataOfOrganization() {
+  return organization;
+}
