b51cd56e497c0515beedd7642affd97f53a8e2a7
ReinerLau <lk850593913@gmail.com>
2024-06-12 08:15:55 +0800

    重来

--- src/inline-function/index.ts
+++
@@ -1,7 +0,0 @@
-function rating(aDriver) {
-  return moreThanFiveLateDeliveries(aDriver) ? 2 : 1;
-}
-
-function moreThanFiveLateDeliveries(aDriver) {
-  return aDriver.numberOfLateDeliveries > 5;
-}
