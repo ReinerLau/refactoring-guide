3d589494ddbb11aaa33b9437954a6a6b01f1236d
ReinerLau <lk850593913@gmail.com>
2024-06-12 09:06:36 +0800

    提炼函数

---
+++ src/extract-function/example-1/after/index.test.ts
@@ -0,0 +1,40 @@
+import { describe, expect, it, vi } from "vitest";
+import { printOwing } from ".";
+
+describe("提炼函数-after", () => {
+  it("print banner", () => {
+    const spy = vi.spyOn(console, "log");
+    const invoice = {
+      orders: [{ amount: 1 }],
+      dueDate: new Date(),
+      customer: "ReinerLau",
+    };
+
+    printOwing(invoice);
+
+    expect(spy).toHaveBeenCalledWith("***********************");
+    expect(spy).toHaveBeenCalledWith("**** Customer Owes ****");
+  });
+
+  it("print details", () => {
+    const spy = vi.spyOn(console, "log");
+    const invoice = {
+      orders: [{ amount: 1 }],
+      dueDate: new Date(),
+      customer: "ReinerLau",
+    };
+
+    printOwing(invoice);
+
+    const today = new Date();
+    const newDueDate = new Date(
+      today.getFullYear(),
+      today.getMonth(),
+      today.getDate() + 30
+    );
+
+    expect(spy).toHaveBeenCalledWith(`name: ${invoice.customer}`);
+    expect(spy).toHaveBeenCalledWith(`amount: 1`);
+    expect(spy).toHaveBeenCalledWith(`due: ${newDueDate.toLocaleDateString()}`);
+  });
+});
