be8240d3bcf90990c0036b0130c474746e375138
ReinerLau <lk850593913@gmail.com>
2024-06-20 17:16:09 +0800

    函数组合成变换-范例-step4

--- src/combine-functions-into-transform/example-1/client-2.ts
+++ src/combine-functions-into-transform/example-1/client-2.ts
@@ -2,8 +2,10 @@ import { acquireReading, enrichReading } from ".";
 
 const rawReading = acquireReading();
 const aReading = enrichReading(rawReading);
-const base = aReading.baseCharge;
-export const taxableCharge = Math.max(0, base! - taxThreshold(aReading.year));
+export const taxableCharge = Math.max(
+  0,
+  aReading.baseCharge! - taxThreshold(aReading.year)
+);
 
 function taxThreshold(year: number) {
   return year;
