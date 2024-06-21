13c4129fa12f6b132d0f59dcdf520f201c04c2d3
ReinerLau <lk850593913@gmail.com>
2024-06-21 10:11:35 +0800

    封装记录-范例-step1

--- src/encapsulate-record/example-1/index.ts
+++ src/encapsulate-record/example-1/index.ts
@@ -3,5 +3,9 @@ export const organization = { name: "Acme Gooseberries", country: "GB" };
 export let result = "";
 const newName = "test";
 
-result += `<h1>${organization.name}</h1>`;
-organization.name = newName;
+result += `<h1>${getRawDataOfOrganization().name}</h1>`;
+getRawDataOfOrganization().name = newName;
+
+function getRawDataOfOrganization() {
+  return organization;
+}
